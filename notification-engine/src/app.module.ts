ecbd67fb38fc4e9282e03b27e02eecb493e95304
rogeriof-zolly <rogeriozolly603@gmail.com>
2023-05-03 23:31:16 -0300

    mongoose installation

--- notification-engine/src/app.module.ts
+++ notification-engine/src/app.module.ts
@@ -3,11 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { SendGridModule } from '@anchan828/nest-sendgrid';
+import { MongooseModule } from '@nestjs/mongoose';
+import { NotificationSchema } from 'interfaces/notification.schema';
 
 @Module({
-  imports: [ConfigModule.forRoot(), SendGridModule.forRoot({
-    apikey: process.env.SENDGRID_API_KEY
-  })],
+  imports: [
+    ConfigModule.forRoot(), 
+    SendGridModule.forRoot({
+      apikey: process.env.SENDGRID_API_KEY
+    }),
+    MongooseModule.forRoot('mongodb://localhost/notification'),
+    MongooseModule.forFeature([{ name: 'Notification', schema: NotificationSchema}])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
